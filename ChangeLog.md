d428446754fe68c404381fcbc3c7abe45b8b6f77
Anastasios Zouzias <zouzias@gmail.com>
2018-04-08 22:28:27 +0200

    init commit

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# Revision history for haskell-essence-func-prog
+
+## 0.1.0.0  -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Main.hs
@@ -0,0 +1,65 @@
+module Main where
+import Numeric
+
+main :: IO ()
+main = putStrLn $ test term0
+
+
+type  Name = String
+
+data  Term =  Var Name
+                       |  Con Int
+                       |  Add Term Term
+                       |  Lam Name Term
+                       |  App Term Term
+
+data Value =  Wrong
+                       |  Num Int
+                       |  Fun (Value -> M Value)
+
+type  Environment =  [(Name, Value)]
+
+
+showval :: Value -> String
+showval Wrong =  "<wrong>"
+showval (Num i) =  show i
+showval (Fun f) =  "<function>"
+
+
+type M a = a
+unitM a = a
+a `bindM` k = k a
+showM a = showval a
+
+
+interp :: Term -> Environment -> M Value
+interp (Var x) e =  llookup x e
+interp (Con i) e =  unitM (Num i)
+interp (Add u v) e =  interp u e `bindM` (\a ->
+                     interp v e `bindM` (\b ->
+                     add a b))
+interp (Lam x  v) e = unitM (Fun (\a -> interp v ((x, a):e)))
+interp (App t u) e = interp t e `bindM` (\f ->
+                                    interp u e `bindM` (\a ->
+                                    apply f a))
+
+
+llookup :: Name -> Environment -> M Value
+llookup x [] =  unitM Wrong
+llookup x ((y, b):e) =  if  x==y  then  unitM b  else  llookup x e
+
+
+add :: Value -> Value -> M Value
+add (Num i) (Num j) =  unitM (Num (i+j))
+add a b =  unitM Wrong
+
+apply :: Value -> Value -> M Value
+apply (Fun k) a = k a
+apply f a = unitM Wrong
+
+test :: Term -> String
+test t =  showM (interp t [])
+
+
+term0 = (App (Lam "x" (Add (Var "x") (Var "x")))
+                   (Add (Con 10) (Con 11)))
