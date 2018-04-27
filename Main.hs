959a066581109d9ca4443676e6b35db5604f602f
Anastasios Zouzias <zouzias@gmail.com>
2018-04-27 11:01:04 +0200

    variation-one: init commit

--- Main.hs
+++ Main.hs
@@ -1,10 +1,10 @@
 module Main where
 import Numeric
-import IdMonad (M, unitM, bindM)
--- import ErrorMonad (M, unitM, bindM, M(Success), M(Error))
+import ErrorMonad (E, unitE, bindE, errorE, E(Success), E(Error))
 
 main :: IO ()
 main = putStrLn $ "Output is: " ++ test term0
+-- main = putStrLn $ "Output is: " ++ test termError
 
 
 type  Name = String
@@ -17,7 +17,7 @@ data  Term =  Var Name
 
 data Value =  Wrong
                        |  Num Int
-                       |  Fun (Value -> M Value)
+                       |  Fun (Value -> E Value)
 
 type  Environment =  [(Name, Value)]
 
@@ -27,41 +27,39 @@ showval Wrong =  "<wrong>"
 showval (Num i) =  show i
 showval (Fun f) =  "<function>"
 
--- Show for IdMonad
-showM a = showval a
 
 -- Show for ErrorMonad
--- showM (Success a) = "Success: " ++ showval a
--- showM (Error s) = "Error: " ++ s
+showE (Success a) = "Success: " ++ showval a
+showE (Error s) = "Error: " ++ s
 
-
-interp :: Term -> Environment -> M Value
+interp :: Term -> Environment -> E Value
 interp (Var x) e =  llookup x e
-interp (Con i) e =  unitM (Num i)
-interp (Add u v) e =  interp u e `bindM` (\a ->
-                     interp v e `bindM` (\b ->
+interp (Con i) e =  unitE (Num i)
+interp (Add u v) e =  interp u e `bindE` (\a ->
+                     interp v e `bindE` (\b ->
                      add a b))
-interp (Lam x  v) e = unitM (Fun (\a -> interp v ((x, a):e)))
-interp (App t u) e = interp t e `bindM` (\f ->
-                                    interp u e `bindM` (\a ->
+interp (Lam x  v) e = unitE (Fun (\a -> interp v ((x, a):e)))
+interp (App t u) e = interp t e `bindE` (\f ->
+                                    interp u e `bindE` (\a ->
                                     apply f a))
 
 
-llookup :: Name -> Environment -> M Value
-llookup x [] =  unitM Wrong
-llookup x ((y, b):e) =  if  x==y  then  unitM b  else  llookup x e
+llookup :: Name -> Environment -> E Value
+llookup x [] =  errorE ("unbounded variable" ++ x)
+llookup x ((y, b):e) =  if  x==y  then  unitE b  else  llookup x e
 
 
-add :: Value -> Value -> M Value
-add (Num i) (Num j) =  unitM (Num (i+j))
-add a b =  unitM Wrong
+add :: Value -> Value -> E Value
+add (Num i) (Num j) =  unitE (Num (i + j))
+add a b =  errorE ("should be numbers" ++ showval a
+                                       ++ "," ++ showval b)
 
-apply :: Value -> Value -> M Value
+apply :: Value -> Value -> E Value
 apply (Fun k) a = k a
-apply f a = unitM Wrong
+apply f a = errorE ("should be function" ++ showval f)
 
 test :: Term -> String
-test t =  showM (interp t [])
+test t =  showE (interp t [])
 
 
 term0 = (App (Lam "x" (Add (Var "x") (Var "x")))
